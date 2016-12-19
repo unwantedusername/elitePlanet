7b03d602ca6c0f3e92d72cc51a316fa9733e803b
unwantedusername <nscheltema@gmail.com>
2016-12-19 20:14:17 +0200

    Add files via upload

---
+++ data_pleiades_barnacles.js
@@ -0,0 +1,743 @@
+// Barnacle locations from NetSlayer's spreadsheet at https://docs.google.com/spreadsheets/d/1bichQJBbL9ygW1xXc3GQhC5KT-o9u05ihWKXebt4BcM/edit#gid=223643326
+// Only confirmed entries listed here.
+// See dataVersion variable for last update date.
+
+// === DATA NOTES ===
+// Planet names must be distinct.
+// POI names per planet must be distinct.
+// The details/poi lists may be empty, but they must exist.
+// Detail values may have new-lines (\n).
+// POI types: O=outpost, S=settlement, C=crash site, B=barnacle
+
+var dataName = "Pleiades/California/WitchHead Known Barnacle Locations etc.";
+var dataVersion = "2016.07.01";
+var data = {
+	"CALIFORNIA SECTOR BA-A E6 4" : {
+		"system" : "CALIFORNIA SECTOR BA-A E6",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : 37.9339,
+				"lon" : -40.4353,
+				"type" : "B"
+			},
+			"BARNACLE #2" : {
+				"lat" : 34.832,
+				"lon" : -40.392,
+				"type" : "B"
+			},
+			"BARNACLE #3" : {
+				"lat" : 30.5814,
+				"lon" : -66.2712,
+				"type" : "B"
+			},
+			"BARNACLE #4" : {
+				"lat" : -37.4567,
+				"lon" : 14.9971,
+				"type" : "B"
+			}
+		}
+	},
+	"CALIFORNIA SECTOR DL-Y D4 C 2" : {
+		"system" : "CALIFORNIA SECTOR DL-Y D4",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : 45.6905,
+				"lon" : -88.1535,
+				"type" : "B"
+			}
+		}
+	},
+	"CALIFORNIA SECTOR DL-Y D4 C 3" : {
+		"system" : "CALIFORNIA SECTOR DL-Y D4",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -21.6941,
+				"lon" : 23.0779,
+				"type" : "B"
+			},
+			"BARNACLE #2" : {
+				"lat" : -34.46,
+				"lon" : -44.4367,
+				"type" : "B"
+			},
+		}
+	},
+	"CALIFORNIA SECTOR DL-Y D4 C 4" : {
+		"system" : "CALIFORNIA SECTOR DL-Y D4",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -29.4314,
+				"lon" : -54.3335,
+				"type" : "B"
+			}
+		}
+	},
+	"CALIFORNIA SECTOR DL-Y D4 C 5" : {
+		"system" : "CALIFORNIA SECTOR DL-Y D4",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -24.7449,
+				"lon" : -64.0429,
+				"type" : "B"
+			}
+		}
+	},
+	"CALIFORNIA SECTOR DL-Y D4 C 6" : {
+		"system" : "CALIFORNIA SECTOR DL-Y D4",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : 10.357,
+				"lon" : -80.2207,
+				"type" : "B"
+			}
+		}
+	},
+	"CALIFORNIA SECTOR LC-V C2-10 A 3" : {
+		"system" : "CALIFORNIA SECTOR LC-V C2-10",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -17.3842,
+				"lon" : -18.7939,
+				"type" : "B"
+			}
+		}
+	},
+	"CALIFORNIA SECTOR LC-V C2-10 B 1" : {
+		"system" : "CALIFORNIA SECTOR LC-V C2-10",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -56.2571,
+				"lon" : 108.4921,
+				"type" : "B"
+			}
+		}
+	},
+	"MEROPE 2 A" : {
+		"system" : "MEROPE",
+		"details" : {
+			"EARTH MASSES" : "0.0017",
+			"RADIUS" : "837KM",
+			"GRAVITY" : "0.10G",
+			"SURFACE TEMP" : "451K",
+			"VOLCANISM" : "IRON MAGMA",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "91.1% ROCK\n8.9% METAL",
+			"ORBITAL PERIOD" : "1.0 D",
+			"SEMI MAJOR AXIS" : "0.00AU",
+			"ORBITAL ECCENTRICITY" : "0.0000",
+			"ORBITAL INCLINATION" : "-0.00 &deg;",
+			"ARG OF PERIAPSIS" : "37.41 &deg;",
+			"ROTATIONAL PERIOD" : "0.9 D (TIDALLY LOCKED)",
+			"AXIAL TILT" : "-29.23 &deg;"
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : 13.4277,
+				"lon" : -161.5145,
+				"type" : "B"
+			}
+		}
+	},
+	"MEROPE 5 C" : {
+		"system" : "MEROPE",
+		"details" : {
+			"EARTH MASSES" : "0.0099",
+			"RADIUS" : "1,478KM",
+			"GRAVITY" : "0.18G",
+			"SURFACE TEMP" : "297K",
+			"VOLCANISM" : "NO VOLCANISM",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "86.0% ROCK\n14.0% METAL",
+			"ORBITAL PERIOD" : "6.6 D",
+			"SEMI MAJOR AXIS" : "0.02AU",
+			"ORBITAL ECCENTRICITY" : "0.0000",
+			"ORBITAL INCLINATION" : "-75.80 &deg;",
+			"ARG OF PERIAPSIS" : "138.32 &deg;",
+			"ROTATIONAL PERIOD" : "6.6 D (TIDALLY LOCKED)",
+			"AXIAL TILT" : "-15.78 &deg;"
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -26.3515,
+				"lon" : -156.4056,
+				"type" : "B"
+			},
+			"BARNACLE #2" : {
+				"lat" : 8.3379,
+				"lon" : -96.1337,
+				"type" : "B"
+			},
+			"BARNACLE #3" : {
+				"lat" : -28.245,
+				"lon" : -109.5219,
+				"type" : "B"
+			}
+		}
+	},
+	"PLEIADES SECTOR IH-V C2-16 C 4" : {
+		"system" : "PLEIADES SECTOR IH-V C2-16",
+		"details" : {
+			"EARTH MASSES" : "0.0102",
+			"RADIUS" : "1,423KM",
+			"GRAVITY" : "0.21G",
+			"SURFACE TEMP" : "374K",
+			"VOLCANISM" : "NO VOLCANISM",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "67.2% ROCK\n32.8% METAL",
+			"ORBITAL PERIOD" : "21.3 D",
+			"SEMI MAJOR AXIS" : "0.11AU",
+			"ORBITAL ECCENTRICITY" : "0.0017",
+			"ORBITAL INCLINATION" : "-1.21 &deg;",
+			"ARG OF PERIAPSIS" : "303.65 &deg;",
+			"ROTATIONAL PERIOD" : "21.3 D (TIDALLY LOCKED)",
+			"AXIAL TILT" : "-11.54 &deg;"
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -1.2540,
+				"lon" : -164.1820,
+				"type" : "B"
+			},
+			"BARNACLE #2" : {
+				"lat" : -3.1300,
+				"lon" : -162.2377,
+				"type" : "B"
+			},
+			"BARNACLE #3" : {
+				"lat" : -0.7678,
+				"lon" : -162.5522,
+				"type" : "B"
+			},
+			"BARNACLE #4" : {
+				"lat" : -0.5601,
+				"lon" : -161.9771,
+				"type" : "B"
+			},
+			"BARNACLE #5" : {
+				"lat" : -1.6100,
+				"lon" : -163.5400,
+				"type" : "B"
+			},
+			"BARNACLE #6" : {
+				"lat" : -1.4193,
+				"lon" : -165.1568,
+				"type" : "B"
+			}
+		}
+	},
+	"PLEIADES SECTOR IH-V C2-16 D 2" : {
+		"system" : "PLEIADES SECTOR IH-V C2-16",
+		"details" : {
+			"EARTH MASSES" : "0.0025",
+			"RADIUS" : "899KM",
+			"GRAVITY" : "0.13G",
+			"SURFACE TEMP" : "380K",
+			"VOLCANISM" : "SILICATE VAPOUR GEYSERS",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "67.2% ROCK\n32.8% METAL",
+			"ORBITAL PERIOD" : "0.4 D",
+			"SEMI MAJOR AXIS" : "0.00AU",
+			"ORBITAL ECCENTRICITY" : "0.1453",
+			"ORBITAL INCLINATION" : "-0.74 &deg;",
+			"ARG OF PERIAPSIS" : "292.04 &deg;",
+			"ROTATIONAL PERIOD" : "0.5 D (TIDALLY LOCKED)",
+			"AXIAL TILT" : "-9.08 &deg;"
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -6.51,
+				"lon" : 132.85,
+				"type" : "B"
+			},
+			"BARNACLE #2" : {
+				"lat" : -4.66,
+				"lon" : 136.69,
+				"type" : "B"
+			},
+		}
+	},
+	"PLEIADES SECTOR IH-V C2-16 D 3" : {
+		"system" : "PLEIADES SECTOR IH-V C2-16",
+		"details" : {
+			"EARTH MASSES" : "0.0020",
+			"RADIUS" : "825KM",
+			"GRAVITY" : "0.12G",
+			"SURFACE TEMP" : "380K",
+			"VOLCANISM" : "SILICATE VAPOUR GEYSERS",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "67.2% ROCK\n32.8% METAL",
+			"ORBITAL PERIOD" : "0.4 D",
+			"SEMI MAJOR AXIS" : "0.00AU",
+			"ORBITAL ECCENTRICITY" : "0.1453",
+			"ORBITAL INCLINATION" : "-0.74 &deg;",
+			"ARG OF PERIAPSIS" : "112.04 &deg;",
+			"ROTATIONAL PERIOD" : "0.5 D (TIDALLY LOCKED)",
+			"AXIAL TILT" : "2.96 &deg;"
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : 5.89,
+				"lon" : -70.95,
+				"type" : "B"
+			},
+			"BARNACLE #2" : {
+				"lat" : 3.31,
+				"lon" : -70.97,
+				"type" : "B"
+			},
+			"BARNACLE #3" : {
+				"lat" : 4.23,
+				"lon" : -69.81,
+				"type" : "B"
+			},
+			"BARNACLE #4" : {
+				"lat" : 28.41,
+				"lon" : 4.8037,
+				"type" : "B"
+			},
+			"BARNACLE #5" : {
+				"lat" : 42.764,
+				"lon" : -97.152,
+				"type" : "B"
+			},
+			"BARNACLE #6" : {
+				"lat" : 42.845,
+				"lon" : -96.98,
+				"type" : "B"
+			},
+		}
+	},
+	"PLEIADES SECTOR JC-U B3-2 1" : {
+		"system" : "PLEIADES SECTOR JC-U B3-2",
+		"details" : {
+			"EARTH MASSES" : "0.0004",
+			"RADIUS" : "476KM",
+			"GRAVITY" : "0.07G",
+			"SURFACE TEMP" : "379K",
+			"VOLCANISM" : "SILICATE VAPOUR GEYSERS",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "66.7% ROCK\n33.3% METAL",
+			"ORBITAL PERIOD" : "0.7 D",
+			"SEMI MAJOR AXIS" : "0.00AU",
+			"ORBITAL ECCENTRICITY" : "0.0935",
+			"ORBITAL INCLINATION" : "9.66 &deg;",
+			"ARG OF PERIAPSIS" : "19.79 &deg;",
+			"ROTATIONAL PERIOD" : "0.7 D (TIDALLY LOCKED)",
+			"AXIAL TILT" : "26.19 &deg;"
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -57.8318,
+				"lon" : 107.4311,
+				"type" : "B"
+			},			
+			"BARNACLE #2" : {
+				"lat" : 19.2500,
+				"lon" : 92.0900,
+				"type" : "B"
+			},
+			"BARNACLE #3" : {
+				"lat" : 20.1833,
+				"lon" : 86.7352,
+				"type" : "B"
+			},
+			"BARNACLE #4" : {
+				"lat" : 20.8600,
+				"lon" : 91.6800,
+				"type" : "B"
+			},
+			"BARNACLE #5" : {
+				"lat" : 20.8600,
+				"lon" : 91.7300,
+				"type" : "B"
+			},
+			"BARNACLE #6" : {
+				"lat" : 21.3348,
+				"lon" : 91.9834,
+				"type" : "B"
+			},
+			"BARNACLE #7" : {
+				"lat" : 21.6104,
+				"lon" : 89.0181,
+				"type" : "B"
+			},
+			"BARNACLE #8" : {
+				"lat" : 21.6200,
+				"lon" : 89.0200,
+				"type" : "B"
+			},
+			"BARNACLE #9" : {
+				"lat" : 21.9135,
+				"lon" : 96.9802,
+				"type" : "B"
+			},
+			"BARNACLE #10" : {
+				"lat" : 22.8000,
+				"lon" : 94.0700,
+				"type" : "B"
+			},
+			"BARNACLE #11" : {
+				"lat" : 25.3363,
+				"lon" : 25.5291,
+				"type" : "B"
+			},
+			"BARNACLE #12" : {
+				"lat" : 2.4138,
+				"lon" : 23.5577,
+				"type" : "B"
+			},
+			"BARNACLE #13" : {
+				"lat" : -0.9981,
+				"lon" : 25.8500,
+				"type" : "B"
+			},
+			"BARNACLE #14" : {
+				"lat" : 9.3400,
+				"lon" : 22.4900,
+				"type" : "B"
+			},
+			"BARNACLE #15" : {
+				"lat" : 21.3401,
+				"lon" : 91.9827,
+				"type" : "B"
+			},
+			"BARNACLE #16" : {
+				"lat" : -21.2700,
+				"lon" : -85.6100,
+				"type" : "B"
+			},
+			"BARNACLE #17" : {
+				"lat" : -8.7979,
+				"lon" : -13.3657,
+				"type" : "B"
+			},
+			"BARNACLE #18" : {
+				"lat" : -58.6949,
+				"lon" : -98.2852,
+				"type" : "B"
+			},
+			"CRASHED ANACONDA" : {
+				"lat" : 20.8736,
+				"lon" : 91.6983,
+				"type" : "C"
+			}
+		},
+	},
+	"PLEIADES SECTOR JC-U B3-2 2" : {
+		"system" : "PLEIADES SECTOR JC-U B3-2",
+		"details" : {
+			"EARTH MASSES" : "0.0003",
+			"RADIUS" : "437KM",
+			"GRAVITY" : "0.06G",
+			"SURFACE TEMP" : "379K",
+			"VOLCANISM" : "SILICATE VAPOUR GEYSERS",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "66.7% ROCK\n33.3% METAL",
+			"ORBITAL PERIOD" : "0.7 D",
+			"SEMI MAJOR AXIS" : "0.00AU",
+			"ORBITAL ECCENTRICITY" : "0.0935",
+			"ORBITAL INCLINATION" : "9.66 &deg;",
+			"ARG OF PERIAPSIS" : "198.79 &deg;",
+			"ROTATIONAL PERIOD" : "0.6 D (TIDALLY LOCKED)",
+			"AXIAL TILT" : "17.54 &deg;"
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : 13.0000,
+				"lon" : 18.2400,
+				"type" : "B"
+			},
+			"BARNACLE #2" : {
+				"lat" : 49.0665,
+				"lon" : 102.9364,
+				"type" : "B"
+			},
+			"BARNACLE #3" : {
+				"lat" : 49.5347,
+				"lon" : 102.0792,
+				"type" : "B"
+			},
+			"BARNACLE #4" : {
+				"lat" : 49.7519,
+				"lon" : 102.1610,
+				"type" : "B"
+			},
+			"BARNACLE #5" : {
+				"lat" : 49.7498,
+				"lon" : 102.7589,
+				"type" : "B"
+			},
+			"BARNACLE #6" : {
+				"lat" : 49.9075,
+				"lon" : 102.7141,
+				"type" : "B"
+			},
+			"BARNACLE #7" : {
+				"lat" : 59.4000,
+				"lon" : -59.7000,
+				"type" : "B"
+			},
+			"BARNACLE #8" : {
+				"lat" : 49.9800,
+				"lon" : 101.5100,
+				"type" : "B"
+			},
+			"BARNACLE #9" : {
+				"lat" : -1.8600,
+				"lon" : -45.1700,
+				"type" : "B"
+			},
+			"BARNACLE #10" : {
+				"lat" : -17.6100,
+				"lon" : -18.0600,
+				"type" : "B"
+			},
+			"BARNACLE #11" : {
+				"lat" : -17.9500,
+				"lon" : -17.2800,
+				"type" : "B"
+			},
+			"BARNACLE #12" : {
+				"lat" : -18.2300,
+				"lon" : -17.3500,
+				"type" : "B"
+			},
+			"BARNACLE #13" : {
+				"lat" : -28.0400,
+				"lon" : 29.5100,
+				"type" : "B"
+			},
+			"BARNACLE #14" : {
+				"lat" : -2.7000,
+				"lon" : 45.2700,
+				"type" : "B"
+			},
+			"BARNACLE #15" : {
+				"lat" : -1.8500,
+				"lon" : 43.6300,
+				"type" : "B"
+			},
+			"BARNACLE #16" : {
+				"lat" : 13.0100,
+				"lon" : 18.2500,
+				"type" : "B"
+			},
+			"BARNACLE #17" : {
+				"lat" : 61.2300,
+				"lon" : -56.0200,
+				"type" : "B"
+			},
+			"BARNACLE #18" : {
+				"lat" : 60.3878,
+				"lon" : -51.8026,
+				"type" : "B"
+			},
+			"BARNACLE #19" : {
+				"lat" : 46.6403,
+				"lon" : -62.9002,
+				"type" : "B"
+			},
+			"BARNACLE #20" : {
+				"lat" : 49.9060,
+				"lon" : 101.5107,
+				"type" : "B"
+			}
+		}
+	},
+	"PLEIONE 11 A" : {
+		"system" : "PLEIONE",
+		"details" : {
+			"EARTH MASSES" : "0.0011",
+			"RADIUS" : "720KM",
+			"GRAVITY" : "0.08G",
+			"SURFACE TEMP" : "194K",
+			"VOLCANISM" : "NO VOLCANISM",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "90.3% ROCK\n9.7% METAL",
+			"ORBITAL PERIOD" : "12.4 D",
+			"SEMI MAJOR AXIS" : "0.01AU",
+			"ORBITAL ECCENTRICITY" : "0.0004",
+			"ORBITAL INCLINATION" : "0.00 &deg;",
+			"ARG OF PERIAPSIS" : "349.15 &deg;",
+			"ROTATIONAL PERIOD" : "-12.4 D",
+			"AXIAL TILT" : "-142.31 &deg;"
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : 0.8185,
+				"lon" : 176.5622,
+				"type" : "B"
+			},
+			"BARNACLE #2" : {
+				"lat" : 2.3189,
+				"lon" : 177.2434,
+				"type" : "B"
+			},
+			"BARNACLE #3" : {
+				"lat" : -6.2472,
+				"lon" : -173.9321,
+				"type" : "B"
+			}
+		}
+	},
+	"WITCH HEAD SECTOR GW-W C1-4 A 4" : {
+		"system" : "WITCH HEAD SECTOR GW-W C1-4",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -65.9886,
+				"lon" : -173.642,
+				"type" : "B"
+			}
+		}
+	},	
+	"WITCH HEAD SECTOR LC-V C2-10 A 4" : {
+		"system" : "WITCH HEAD SECTOR LC-V C2-10",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -16.5359,
+				"lon" : 151.8175,
+				"type" : "B"
+			}
+		}
+	},	
+	"WITCH HEAD SECTOR LC-V C2-10 A 6" : {
+		"system" : "WITCH HEAD SECTOR LC-V C2-10",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -24.4643,
+				"lon" : -26.8704,
+				"type" : "B"
+			}
+		}
+	},
+	"WITCH HEAD SECTOR LC-V C2-10 B 1" : {
+		"system" : "WITCH HEAD SECTOR LC-V C2-10",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : 4.0266,
+				"lon" : -40.045,
+				"type" : "B"
+			}
+		}
+	},
+	"WITCH HEAD SECTOR LC-V C2-10 B 2" : {
+		"system" : "WITCH HEAD SECOTR LC-V C2-10",
+		"details" : {
+		},
+		"poi" : {
+			"BARNACLE #1" : {
+				"lat" : -0.5026,
+				"lon" : -163.3102,
+				"type" : "B"
+			}
+		}
+	},
+	// settled locations (no barnacles so far)
+	"MAIA A 2 A" : {
+		"system" : "MAIA",
+		"details" : {
+			"EARTH MASSES" : "0.0074",
+			"RADIUS" : "1,032KM",
+			"GRAVITY" : "0.28G",
+			"SURFACE TEMP" : "1,088K",
+			"VOLCANISM" : "IRON MAGMA",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "100.0% METAL",
+			"ORBITAL PERIOD" : "1.2 D",
+			"SEMI MAJOR AXIS" : "0.01AU",
+			"ORBITAL ECCENTRICITY" : "0.0033",
+			"ORBITAL INCLINATION" : "-0.02 &deg;",
+			"ARG OF PERIAPSIS" : "49.81 &deg;",
+			"ROTATIONAL PERIOD" : "1.2 D (TIDALLY LOCKED)",
+			"AXIAL TILT" : "-5.01 &deg;"
+		},
+		"poi" : {
+			"DARNIELLE'S PROGRESS" : {
+				"lat" : 22.0208,
+				"lon" : -38.4533,
+				"type" : "O"
+			}
+		}
+	},
+	"MAIA B 1 D" : {
+		"system" : "MAIA",
+		"details" : {
+			"EARTH MASSES" : "0.0050",
+			"RADIUS" : "1,196KM",
+			"GRAVITY" : "0.14G",
+			"SURFACE TEMP" : "87K",
+			"VOLCANISM" : "IRON MAGMA",
+			"ATMOSPHERE TYPE" : "NO ATMOSPHERE",
+			"COMPOSITION" : "91.0% ROCK\n9.0% METAL",
+			"ORBITAL PERIOD" : "0.7 D",
+			"SEMI MAJOR AXIS" : "0.00AU",
+			"ORBITAL ECCENTRICITY" : "0.0708",
+			"ORBITAL INCLINATION" : "0.69 &deg;",
+			"ARG OF PERIAPSIS" : "272.51 &deg;",
+			"ROTATIONAL PERIOD" : "1.0 D (TIDALLY LOCKED)",
+			"AXIAL TILT" : "-24.69 &deg;"
+		},
+		"poi" : {
+			"PAVLOU KEEP" : {
+				"lat" : 12.7808,
+				"lon" : 107.3216,
+				"type" : "S"
+			}
+		}
+	},
+	// generic planet for linking
+	"GENERIC" : {
+		"system" : "UNSPECIFIED",
+		"details" : {
+		},
+		"poi" : {
+		}
+	}
+};
+
+// simple data integrity test
+var errorLog = "";
+for (var p in data) {
+	if (!data[p].hasOwnProperty("system"))
+		errorLog += "Planet ("+p+") has no 'system' property.\n";
+	if (!data[p].hasOwnProperty("details"))
+		errorLog += "Planet ("+p+") has no 'details' property.\n";
+	if (!data[p].hasOwnProperty("poi"))
+		errorLog += "Planet ("+p+") has no 'poi' property.\n";
+	else {
+		for (var poi in data[p]["poi"]) {
+			if (!data[p]["poi"][poi].hasOwnProperty("lat"))
+				errorLog += "Planet ("+p+") with POI ("+poi+") has no 'lat' property.\n";
+			if (!data[p]["poi"][poi].hasOwnProperty("lon"))
+				errorLog += "Planet ("+p+") with POI ("+poi+") has no 'lon' property.\n";
+			if (!data[p]["poi"][poi].hasOwnProperty("type"))
+				errorLog += "Planet ("+p+") with POI ("+poi+") has no 'type' property.\n";
+		}
+	}
+}
+if (errorLog != "")
+	alert(errorLog);
